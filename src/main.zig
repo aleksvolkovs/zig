4ab66d8eb986f0df05c85e23e192deb977455833
Rscer <AleksDevsCode@gmail.com>
2024-08-06 22:52:07 +0300

    main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,13 @@
 const print = @import("std").debug.print;
 
+//Global
+const vat: f32 = 1.2;
+
 pub fn main() !void {
     print("{d:.2}\n", .{ticketCostAfterTax(10)});
 }
 
 fn ticketCostAfterTax(cost: f32) f32 {
-    const vat: f32 = 1.2; //20% tax
     const final_cost = cost * vat;
     return final_cost;
 }
